e1b08d1ff116ab268df1d691353fdc8cf15abb37
Borland <aborland@tubecityims.com>
2017-07-03 13:35:16 -0400

    Initial Commit

---
+++ QMS_Service.py
@@ -0,0 +1,30 @@
+from requests import Session
+from requests_ntlm import HttpNtlmAuth
+from zeep import Client
+from zeep.transports import Transport
+import time
+
+class QMS_Service(object):
+    def __init__(self, server_name, username, password):
+        self.key_exp = 0
+        session = Session()
+        session.auth = HttpNtlmAuth(
+            username,
+            password
+        )
+        self.c = Client(
+            f"http://{server_name}:4799/QMS/Service",
+            transport = Transport(session = session)
+        )
+        self.api = self.c.service
+        self.refresh_key()
+        self.services = self.api.GetServices(["All"])
+
+    def refresh_key(self):
+        if time.time() >= self.key_exp:
+            self.key_exp = time.time()+60
+            self.service_key = self.c.service.GetTimeLimitedServiceKey()
+            print(f"Checked out service key: {self.service_key}")
+            self.c.transport.session.headers.update(
+                {"X-Service-Key": self.service_key}
+            )
